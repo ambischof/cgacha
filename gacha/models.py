a596e51effe9e2405b953f99189515b3c96fbcca
Anne Bischof <7795449+ambischof@users.noreply.github.com>
2025-01-30 02:01:02 -0600

    Adding rolling function and tests

--- gacha/models.py
+++ gacha/models.py
@@ -5,7 +5,7 @@ from .lib import rarity
 class Item(models.Model):
   name = models.CharField(max_length=30)
   
-  # not how labels will work? documentation is scarce.
+  # not sure how labels will work? documentation is scarce.
   class Rarities(models.IntegerChoices): 
     ONE = 1, '★',
     TWO = 2, '★★',
@@ -15,6 +15,7 @@ class Item(models.Model):
 
   rarity = models.IntegerField(choices=Rarities, default=1)
   
+  @staticmethod
   def get_random_item():
     rarityVal = rarity.get_random_rarity()
     return Item.objects.filter(rarity=rarityVal).order_by('?')[:1][0]
@@ -29,8 +30,34 @@ class Account(models.Model):
   credits = models.IntegerField(default=100)
   items = models.ManyToManyField(Item, through="AccountItem", through_fields=("username", "item"))
 
+
+
 # relation between user and item
 class AccountItem(models.Model):
   username = models.ForeignKey(Account, on_delete=models.CASCADE)
   item = models.ForeignKey(Item, on_delete=models.CASCADE)
   acquired = models.DateTimeField(auto_now_add=True)
+  
+  ''' the bread and butter of the app, trading credits for an item '''
+  @staticmethod
+  def roll(account):
+    if (account.credits < 1):
+      raise Exception('Not enough Credits')
+
+    account.credits -= 1
+    account.save()
+    item = Item.get_random_item()
+    try:
+      association = AccountItem(username=account, item=item)
+      association.save()
+    
+    #TODO: figure out transactions to make sure credits only are
+    # taken when roll is successful
+    except Exception as e:
+      account.credits += 1
+      account.save()
+      raise e
+
+    return item
+
+    
--- gacha/tests.py
+++ gacha/tests.py
@@ -1,7 +1,15 @@
 from django.test import TestCase
 from .lib import rarity
-from .models import Item
+from .models import Item, Account, AccountItem
 import re
+
+def make_items():
+  items = []
+  for x in range(1,6): 
+      for y in range(1,4):
+        items.append(Item.objects.create(name="item "+str(x)+str(y), rarity=x))
+  return items
+
 
 class RarityTests(TestCase): 
   def tests_are_spreading_correctly(self):
@@ -32,13 +40,27 @@ class ItemTests(TestCase):
   # for each 5 rarities, create 3 items
   @classmethod
   def setUpTestData(cls):
-    cls.items = []
-    for x in range(1,6): 
-      for y in range(1,4):
-        cls.items.append(Item.objects.create(name="item "+str(x)+str(y), rarity=x))
+    cls.items = make_items()
+    
 
   def test_can_get_random_item(self):
     rando = Item.get_random_item()
     reg = re.compile('^item \d\d')
     self.assertTrue(reg.match(rando.name))
     
+
+class AccountItemTests(TestCase):
+  @classmethod
+  def setUpTestData(cls):
+    cls.user = Account(username="Sally")
+    cls.broke_user = Account(username="brokeJoe", credits=0)
+    cls.items = make_items()
+  
+  def test_roll(self):
+    item = AccountItem.roll(self.user)
+    self.assertEqual(self.user.credits, 99)
+    self.assertEqual(item, self.user.items.all()[0])
+
+  def test_credit_check(self):
+    with self.assertRaisesMessage(Exception, 'Not enough Credits'):
+      item = AccountItem.roll(self.broke_user)
