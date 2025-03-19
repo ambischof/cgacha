cfbec1ca514b2305a1a9dc1d0cadedd23e72fa36
Anne Bischof <7795449+ambischof@users.noreply.github.com>
2025-03-18 22:55:22 -0500

    Updating add_items to only add items

--- add_items.py
+++ add_items.py
@@ -1,17 +1,17 @@
-# script for adding the items back when I'd like to nuke the db
+# script for populating the items on clean development. (or after a flush)
 
 from gacha import models
 import json
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 with open('items.json', 'r') as file:
     items = json.load(file)
 
 
 def addItems():
-  # assumes you named your user defaultuser
-  user = User.objects.get(username='defaultuser')
-  models.Account.objects.create(user=user)
+  # assumes you named your superuser defaultuser
+  # user = User.objects.get(username='defaultuser')
+  # models.Account.objects.create(user=user)
 
   for i in items: 
     models.Item.objects.create(
